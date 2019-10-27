01d963eaa2f9cc2717e7ed8896cd9be2f58a3eb4
Raymundo Vásquez Ruiz <raymundo.vr@protonmail.com>
2019-10-27 19:01:08 +0100

    added step-definition

--- src/flow-definition.ts
+++ src/flow-definition.ts
@@ -1,9 +1,25 @@
-class FlowDefinition {
-  id: any;
-  description: string;
+import { StepDefinition } from "./step-definition";
 
-  constructor(id: any, description: string) {
-    this.id = id;
-    this.description = description;
-  }
+export class FlowDefinition {
+    public id: any;
+    public description: string;
+    private _steps: { [key: string]: any } = {};
+
+    constructor(id: any, description: string) {
+	this.id = id;
+	this.description = description;
+	this._steps = {};
+    }
+
+    addStep(step: StepDefinition) {
+	this._steps[step.id] = step;
+    }
+
+    removeStepById(stepId: string) {
+	delete this._steps[stepId];
+    }
+
+    get steps(): object {
+	return this._steps;
+    }
 }
