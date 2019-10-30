ef2e4ef38871e575a030ee8bd9623bf316926881
Raymundo Vásquez Ruiz <raymundo.vr@protonmail.com>
2019-10-30 23:04:48 +0100

    added step methods and tests

--- __tests__/flow-definition.test.ts
+++ __tests__/flow-definition.test.ts
@@ -1,7 +1,8 @@
 import {FlowDefinition, createFlowDefinition } from '../src/flow-definition';
+import { StepDefinition, createStepDefinition } from '../src/step-definition';
 
 describe("FlowDefinition", () => {
-    test("::createFlowDefinition", () => {
+    test("::createFlowDefinition()", () => {
 	expect(
 	    createFlowDefinition('test', "Test")
 	).toMatchObject({
@@ -10,5 +11,22 @@ describe("FlowDefinition", () => {
 	    transitions: {},
 	    startStep: null
 	});
+    });
+
+    test("::setStartStep()", () => {
+	let start = createStepDefinition('step', 'Step');
+	let flow = createFlowDefinition('test', 'Test').
+	    setStartStep(start);
+	expect(flow.startStep).toBe(start);
+	expect(Object.keys(flow.steps).length).toBe(1);
+    });
+
+    test("::addStep().after()", () => {
+    	let flow = createFlowDefinition('test', 'test')
+    	    .setStartStep(createStepDefinition('start', 'start'))
+    	    .addStep(createStepDefinition('a', 'a')).afterStepWithId('start');
+	expect(Object.keys(flow.steps).length).toBe(2);
+	expect(Object.keys(flow.transitions).length).toBe(1);
+	expect(Object.keys(flow.transitions)[0]).toBe('start-a');
     });
 });
--- src/flow-definition.ts
+++ src/flow-definition.ts
@@ -9,12 +9,24 @@ export interface FlowDefinition {
     steps: { [key: string] : StepDefinition };
     startStep: StepDefinition | null;
     addStep: Function;
+    setStartStep: Function;
+}
+
+function addStepToFlow(flow: FlowDefinition, step: StepDefinition) {
+    flow.steps[step.id] = step;
+    return flow;
+}
+
+function addTransitionToFlow(flow: FlowDefinition, transition: Transition) {
+    let id = `${transition.origin.id}-${transition.destination.id}`;
+    flow.transitions[id] = transition;
+    return flow;
 }
 
 function createTransition(flowDefinition: FlowDefinition,
 			  origin: StepDefinition,
 			  destination: StepDefinition,
-			  condition = null): Transition {
+			  condition = null) : Transition {
     return {
 	flowDefinition,
 	origin,
@@ -25,18 +37,38 @@ function createTransition(flowDefinition: FlowDefinition,
 
 function addStepInFlowAfter(flow: FlowDefinition,
 			    step: StepDefinition) {
-    return function(after: StepDefinition, condition = null) : Transition {
-	return createTransition(flow, after, step, condition);
+    return function(after: StepDefinition, condition = null) : FlowDefinition {	
+	return addTransitionToFlow(
+	    flow,
+	    createTransition(flow, after, step, condition)
+	);
     };
 }
 
 function addStepInFlowBefore(flow: FlowDefinition,
 			     step: StepDefinition) {
-    return function(before: StepDefinition, condition = null) : Transition {
-	return createTransition(flow, step, before, condition);	
+    return function(before: StepDefinition, condition = null) : FlowDefinition {
+	return addTransitionToFlow(
+	    flow,
+	    createTransition(flow, step, before, condition)
+	);	
     };
 }
 
+function setFlowStartStep(flow: FlowDefinition,
+			  step: StepDefinition) {
+    flow = addStepToFlow(flow, step);
+    flow.startStep = step;
+    return flow;
+}
+
+function getStepByIdInFlowOrFail(flow: FlowDefinition, id: any) {
+    let step = flow.steps[id];
+    if (!step) {
+	throw new Error(`Cannot find step with ID ${id}`);
+    }
+    return step;
+}
 
 export function createFlowDefinition(id: any, description: string): FlowDefinition {
     return {
@@ -46,10 +78,26 @@ export function createFlowDefinition(id: any, description: string): FlowDefiniti
 	steps: {},
 	startStep: null,
         addStep: function(step: StepDefinition) {
+	    let flow = addStepToFlow(this, step);
 	    return {
-		after: addStepInFlowAfter(this, step),
-		before: addStepInFlowBefore(this, step)
-	    }
+		afterStep: function (after: StepDefinition, condition = null) {
+		    return addStepInFlowAfter(flow, step)(after, condition);
+		},
+		beforeStep: function (before: StepDefinition, condition = null) {
+		    return addStepInFlowBefore(flow, step)(before, condition);
+		},
+		afterStepWithId: function(id: any, condition = null) {
+		    let after = getStepByIdInFlowOrFail(flow, id);
+		    return addStepInFlowAfter(flow, step)(after, condition);
+		},
+		beforeStepWithId: function(id: any, condition = null) {
+		    let before = getStepByIdInFlowOrFail(flow, id);
+		    return addStepInFlowBefore(flow, step)(before, condition);
+		}
+	    };
+	},
+	setStartStep: function (step: StepDefinition) {
+	    return setFlowStartStep(this, step);
 	}
     };
 
