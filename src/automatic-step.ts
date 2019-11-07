4f81b271acd2c92bc729082b8cea3404869cc790
Raymundo Vásquez Ruiz <raymundo.vr@protonmail.com>
2019-11-07 22:05:39 +0100

    added command step definition

---
+++ src/automatic-step.ts
@@ -0,0 +1,12 @@
+import { StepDefinition } from "./step-definition";
+import { Access } from "./step-access";
+
+
+export class CommandStep extends StepDefinition {
+    public commandLine: string;
+
+    constructor(id: any, name: string, commandLine: string, access: Array<Access> = []) {
+        super(id, name, access);
+        this.commandLine = commandLine;
+    }
+}
